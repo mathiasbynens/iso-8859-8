4203fbfa4b7b83bc66a26fc7d1bfe9cf3dae353d
Mathias Bynens <mathias@qiwi.be>
2016-10-24 19:29:10 +0200

    Release v1.0.0

--- iso-8859-8.js
+++ iso-8859-8.js
@@ -1,4 +1,4 @@
-/*! https://mths.be/iso-8859-8 v0.1.2 by @mathias | MIT license */
+/*! https://mths.be/iso-8859-8 v1.0.0 by @mathias | MIT license */
 ;(function(root) {
 
 	// Detect free variables `exports`.
@@ -128,7 +128,7 @@
 			'iso_8859-8:1988',
 			'visual'
 		],
-		'version': '0.1.2'
+		'version': '1.0.0'
 	};
 
 	// Some AMD build optimizers, like r.js, check for specific condition patterns
