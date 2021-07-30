e5464ced20a2d040d6721af225a512448141d47a
Mathias Bynens <mathias@qiwi.be>
2021-07-30 13:05:45 +0200

    Release v1.1.0

--- iso-8859-8.js
+++ iso-8859-8.js
@@ -1,4 +1,4 @@
-/*! https://mths.be/iso-8859-8 v1.0.2 by @mathias | MIT license */
+/*! https://mths.be/iso-8859-8 v1.1.0 by @mathias | MIT license */
 ;(function() {
 
 	const stringFromCharCode = String.fromCharCode;
@@ -104,7 +104,7 @@
 			'iso_8859-8:1988',
 			'visual'
 		],
-		version: '1.0.2',
+		version: '1.1.0',
 	};
 
 	module.exports = iso88598;
